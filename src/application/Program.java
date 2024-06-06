b67fa0fddc51e2c016d2c029ee32aeb8671dbde3
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-06-06 16:26:11 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@
  */
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 /**
@@ -12,8 +13,8 @@ import boardgame.Position;
  */
 public class Program {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println(position);
+        
+        Board board = new Board(8, 8);
     }
     
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author pedro
+ */
+public class Piece {
+    
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+    
+    
+}
