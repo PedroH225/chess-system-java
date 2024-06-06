7ed0cd098e03de029079f5be1acf822aee09a2e6
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-06-06 16:11:14 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,13 +4,16 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author pedro
  */
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
     
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position() {
+    }
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+    
+    public String toString() {
+        return row + ", " + column;
+    }
+}
